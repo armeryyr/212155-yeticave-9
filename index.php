6c41fc47b5ed392ef733d5008e129f736351a4cd
Юрий <bobresh@mail.ru>
2019-04-15 20:34:42 +0400

    Добавляет итерацию по массивам

--- index.php
+++ index.php
@@ -109,21 +109,19 @@ $user_name = 'Юрий'; // укажите здесь ваше имя
                 ]
             ];  ?>
             <?php
-                $index = 0;
-                $num = count($lots);
-                while($index < $num):
+                foreach($lots as $key => $value):
             ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
+                    <img src="<?=$value['URL картинки'];?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <span class="lot__category"></span><?=$value['Категория'];?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value['Название'];?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$value['Цена'];?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -131,6 +129,7 @@ $user_name = 'Юрий'; // укажите здесь ваше имя
                     </div>
                 </div>
             </li>
+            <?php endforeach;?>
         </ul>
     </section>
 </main>
