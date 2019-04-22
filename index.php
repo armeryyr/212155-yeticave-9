4280bb944c0d3b481ac0d3c30d0dc2510f426e95
Юрий <bobresh@mail.ru>
2019-04-22 13:48:56 +0400

    Пишем первую функцию с первой правкой

--- index.php
+++ index.php
@@ -16,6 +16,20 @@ $user_name = 'Юрий'; // укажите здесь ваше имя
 
 <header class="main-header">
     <div class="main-header__container container">
+        <?php
+            ini_set('error_reporting', E_ALL);
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+        ?>
+        <?php $price = 200.85; ?>
+        <?php function formatting_number($price){
+            $price = ceil($price);
+            if ($price >= 1000){
+                $price = number_format($price, 0, ',', ' ');
+            }
+            return $price . " " . "₽";
+        }
+        ?>
         <h1 class="visually-hidden">YetiCave</h1>
         <a class="main-header__logo">
             <img src="../img/logo.svg" width="160" height="39" alt="Логотип компании YetiCave">
@@ -30,7 +44,7 @@ $user_name = 'Юрий'; // укажите здесь ваше имя
            <?php if($is_auth == 1):?>
             <div class="user-menu__logged">
                 <p><?php $user_name = 'Юрий'; ?></p>
-                <a class="user-menu__bets" href="pages/my-bets.html"><?= $user_name; ?></a>
+                <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
                 <a class="user-menu__logout" href="#">Выход</a>
             </div>
             <?php else: ?>
@@ -73,52 +87,43 @@ $user_name = 'Юрий'; // укажите здесь ваше имя
         <div class="lots__header">
             <h2>Открытые лоты</h2>
         </div>
-        <?php $price = 1000.85; ?>
-        <?php function formatting_number($price){
-            if ($price < 1000){
-                return $price = $price . " ";
-            }
-            ceil($price);
-            return $price = $nombre_format_francais = number_format($price, 0, ',', ' ') . " ";
-            }
-        ?>
-        <ul class="lots__list">
+         <ul class="lots__list">
             <?php $lots = [
                 [
-                    'Название' => '2014 Rossignol District Snowboard',
-                    'Категория' => 'Доски и лыжи',
-                    'Цена' => '10999',
-                    'URL картинки' => 'img/lot-1.jpg'
+                    'name' => '2014 Rossignol District Snowboard',
+                    'category' => 'Доски и лыжи',
+                    'price' => '10999',
+                    'url_img' => 'img/lot-1.jpg'
                 ],
                 [
-                    'Название' => 'DC Ply Mens 2016/2017 Snowboard',
-                    'Категория' => 'Доски и лыжи',
-                    'Цена' => '159999',
-                    'URL картинки' => 'img/lot-2.jpg'
+                    'name' => 'DC Ply Mens 2016/2017 Snowboard',
+                    'category' => 'Доски и лыжи',
+                    'price' => '159999',
+                    'url_img' => 'img/lot-2.jpg'
                 ],
                 [
-                    'Название' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-                    'Категория' => 'Крепления',
-                    'Цена' => '8000',
-                    'URL картинки' => 'img/lot-3.jpg'
+                    'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+                    'category' => 'Крепления',
+                    'price' => '8000',
+                    'url_img' => 'img/lot-3.jpg'
                 ],
                 [
-                    'Название' => 'Ботинки для сноуборда DC Mutiny Charocal',
-                    'Категория' => 'Ботинки',
-                    'Цена' => '10999',
-                    'URL картинки' => 'img/lot-4.jpg'
+                    'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+                    'category' => 'Ботинки',
+                    'price' => '10999',
+                    'url_img' => 'img/lot-4.jpg'
                 ],
                 [
-                    'Название' => 'Куртка для сноуборда DC Mutiny Charocal',
-                    'Категория' => 'Одежда',
-                    'Цена' => '7500',
-                    'URL картинки' => 'img/lot-5.jpg'
+                    'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+                    'category' => 'Одежда',
+                    'price' => '7500',
+                    'url_img' => 'img/lot-5.jpg'
                 ],
                 [
-                    'Название' => 'Маска Oakley Canopy',
-                    'Категория' => 'Разное',
-                    'Цена' => '5400',
-                    'URL картинки' => 'img/lot-6.jpg'
+                    'name' => 'Маска Oakley Canopy',
+                    'category' => 'Разное',
+                    'price' => '5400',
+                    'url_img' => 'img/lot-6.jpg'
                 ]
             ];  ?>
             <?php
@@ -126,15 +131,15 @@ $user_name = 'Юрий'; // укажите здесь ваше имя
             ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$value['URL картинки'];?>" width="350" height="260" alt="">
+                    <img src="<?=$value['url_img'];?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"></span><?=$value['Категория'];?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value['Название'];?></a></h3>
+                    <span class="lot__category"></span><?=$value['category'];?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value['name'];?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=formatting_number($price);?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=formatting_number($price);?></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -152,10 +157,10 @@ $user_name = 'Юрий'; // укажите здесь ваше имя
     <nav class="nav">
         <ul class="nav__list container">
             <?php
-                foreach ($categories as $category):
+                foreach ($categories as $value):
             ?>
             <li class="nav__item">
-                <a href="pages/all-lots.html"><?=$categories[$category];?></a>
+                <a href="pages/all-lots.html"><?=$category;?></a>
             </li>
             <?php endforeach; ?>
         </ul>
